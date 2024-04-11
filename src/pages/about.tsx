9dd7959226defa1ed18accb9dc266a8c27729151
slee692 <slee692@illinois.edu>
2024-04-11 11:15:14 -0500

    edit about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -23,99 +23,98 @@ import {
 } from "framer-motion";
 
 export default function About() {
-  const [scope1, animate1] = useAnimate();
-
-  const handleAnimate1 = async () => {
-    animate1(
-      scope1.current,
-      {
-        scale: 1.1,
-      },
-      {
-        repeat: Infinity,
-        repeatType: "mirror",
-        duration: 2,
-      }
-    );
-  };
-  const [scope2, animate2] = useAnimate();
-
-  const handleAnimate2 = async () => {
-    animate1(
-      scope2.current,
-      {
-        scale: 1.1,
-      },
-      {
-        repeat: Infinity,
-        repeatType: "mirror",
-        duration: 2,
-      }
-    );
-  };
-
+  const ref = React.useRef(null);
+  const carouselAnimation = useAnimation();
+  const inView = useInView(ref, { once: true });
   React.useEffect(() => {
-    handleAnimate1();
-    handleAnimate2();
-  });
-
+    if (inView) {
+      carouselAnimation.start("visible");
+    }
+  }, [inView]);
   return (
     <div className="bg-notion">
       <NavigationBar />
-      <div className="flex flex-col p-20">
-        <CardTitle className="text-left text-5xl pt-9 pb-12">
-          Snapshot of my Journey
-        </CardTitle>
-        <div className="flex flex-row">
-          <div className="flex flex-col gap-12">
-            <div className="w-[347px] flex flex-col gap-2">
-              <h3 className="font-bold font-opensans text-[24px]">
-                Motorola Solutions Inc.
-              </h3>
-              <p className="font-opensans text-[24px]">
-                Software Engineer Intern (In Progress)
-              </p>
-            </div>
-            <div className="w-[347px] flex flex-col gap-2">
-              <h3 className="font-bold font-opensans text-[24px]">
-                State Farm RDC
-              </h3>
-              <p className="font-opensans text-[24px]">
-                Technology Intern | NLP Voice
-              </p>
-            </div>
-            <div className="w-[347px] flex flex-col gap-2">
-              <h3 className="font-bold font-opensans text-[24px]">
-                Hack4Impact
-              </h3>
-              <p className="font-opensans text-[24px]">
-                Software Developer | Web Development
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <div className="pl-[25rem]">
-              <motion.div ref={scope1}>
+      <div className="flex flex-col p-20" ref={ref}>
+        <AnimatePresence>
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, y: 75 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            animate={carouselAnimation}
+            initial="hidden"
+            transition={{ duration: 2, delay: 0.35 }}
+          >
+            <CardTitle className="text-left text-5xl pt-9 pb-12">
+              My journey is about finding meaningful ways to{" "}
+              <span className="text-[#2876FA]">positively impact</span> the
+              community{" "}
+            </CardTitle>
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-col gap-12">
+                <div className="flex flex-col gap-2">
+                  <h3 className="font-bold font-opensans text-[24px]">
+                    Motorola Solutions Inc.
+                  </h3>
+                  <p className="font-opensans text-[24px]">
+                    Software Engineer Intern (In Progress)
+                  </p>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <h3 className="font-bold font-opensans text-[24px]">
+                    State Farm RDC
+                  </h3>
+                  <p className="font-opensans text-[24px]">
+                    Technology Intern | NLP Voice
+                  </p>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <h3 className="font-bold font-opensans text-[24px]">
+                    <Link
+                      href={"https://uiuc.hack4impact.org/"}
+                      target="_blank"
+                      className="hover:text-[#2876FA]"
+                    >
+                      Hack4Impact
+                    </Link>
+                  </h3>
+                  <p className="font-opensans text-[24px]">
+                    Software Developer | Web Development
+                  </p>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <h3 className="font-bold font-opensans text-[24px]">
+                    <Link
+                      href={
+                        "https://cs.illinois.edu/broadening-participation-computing/programs/csambassadors/participants/45409"
+                      }
+                      target="_blank"
+                      className="hover:text-[#2876FA]"
+                    >
+                      CS STARS / Crowd Dynamics Lab
+                    </Link>
+                  </h3>
+                  <p className="font-opensans text-[24px]">
+                    Student Researcher | Data Visualization
+                  </p>
+                </div>
+              </div>
+              <div className="px-12 md:px-24">
                 <Image
-                  src={"/about1.png"}
-                  alt="image of presentation"
-                  width={300}
-                  height={300}
+                  src={"/h4i.JPG"}
+                  alt="img of favorite people"
+                  className="rounded-3xl"
+                  width={650}
+                  height={650}
                 ></Image>
-              </motion.div>
+                <p className="font-opensans text-[24px] text-center p-4">
+                  Looking for{" "}
+                  <span className="text-[#2876FA]">inspiration</span> 🔍
+                </p>
+              </div>
             </div>
-            <div className="pl-[50rem]">
-              <motion.div ref={scope2}>
-                <Image
-                  src={"/about2.png"}
-                  alt="image of presentation"
-                  width={300}
-                  height={300}
-                ></Image>
-              </motion.div>
-            </div>
-          </div>
-        </div>
+          </motion.div>
+        </AnimatePresence>
       </div>
     </div>
   );
